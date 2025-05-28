1038cfe02fd2c32040c5c0f9b1d9327352d78efc
Marcos Vinicius de Assis <63564472+MarcosAssisDev@users.noreply.github.com>
2025-05-28 00:19:43 -0300

    Update apiService.js

--- rental-frontend/src/services/apiService.js
+++ rental-frontend/src/services/apiService.js
@@ -1,8 +1,9 @@
 // src/services/apiService.js
 import axios from 'axios';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000/api', // URL base da sua API backend
+    baseURL: baseURL, 
     headers: {
         'Content-Type': 'application/json',
     },
