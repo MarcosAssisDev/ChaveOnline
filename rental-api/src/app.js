4f52b7fbe255173dbf3959f8cce20933e1040ddd
Marcos Vinicius de Assis <63564472+MarcosAssisDev@users.noreply.github.com>
2025-05-28 09:46:32 -0300

    Update app.js

--- rental-api/src/app.js
+++ rental-api/src/app.js
@@ -13,9 +13,6 @@ const authMiddleware = require('./middleware/authMiddleware');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Isso permitirá requisições de qualquer origem.
-// Para mais segurança em produção, você pode configurar origens específicas:
-// app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(cors());
 
 // Middleware para parsear JSON no corpo das requisições
@@ -29,9 +26,6 @@ app.use('/api/reservations', authMiddleware, reservationRoutes);
 app.use('/api/metrics', authMiddleware, metricRoutes);
 app.use('/api/apartments', authMiddleware, apartmentRoutes); 
 app.use('/api/contacts', authMiddleware, contactRoutes);
-
-// (Opcional) Servir arquivos estáticos para o frontend (quando o tivermos)
-// app.use(express.static(path.join(__dirname, '..', 'public'))); // Supondo que o frontend esteja em 'public'
 
 app.get('/', (req, res) => {
     res.send('Rental Management API is running!');
