3210d350f0440cb2a294683241edd77114b37682
Marcos Vinicius de Assis <63564472+MarcosAssisDev@users.noreply.github.com>
2025-05-28 09:42:34 -0300

    Update apartmentController.js

--- rental-api/src/controllers/apartmentController.js
+++ rental-api/src/controllers/apartmentController.js
@@ -1,8 +1,6 @@
-// src/controllers/apartmentController.js
 const db = require('../services/db');
 
-// Helper function para rodar queries que retornam múltiplos resultados
-// (Se você centralizou essa função, pode importá-la)
+
 function allAsync(sql, params = []) {
     return new Promise((resolve, reject) => {
         db.all(sql, params, (err, rows) => {
@@ -36,9 +34,3 @@ exports.listApartments = async (req, res) => {
         res.status(500).json({ error: 'Failed to retrieve apartments' });
     }
 };
-
-// Futuramente, você pode adicionar aqui:
-// exports.createApartment = async (req, res) => { ... };
-// exports.getApartmentById = async (req, res) => { ... };
-// exports.updateApartment = async (req, res) => { ... };
-// exports.deleteApartment = async (req, res) => { ... };
